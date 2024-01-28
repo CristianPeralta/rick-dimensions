a3d5ff8e2e5ec18275606224eb1bf69fcce94d95
Cristian Peralta <cbperaltasegura@gmail.com>
2024-01-28 16:55:59 -0500

    feat: Add dtos

--- src/dimensions/dto/create-dimension.dto.ts
+++ src/dimensions/dto/create-dimension.dto.ts
@@ -1,1 +1,44 @@
-export class CreateDimensionDto {}
+import { IsString, IsOptional, IsBoolean, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+enum DangerLevelEnum {
+    LOW = 'Low',
+    MEDIUM = 'Medium',
+    HIGH = 'High',
+}
+  
+
+export class CreateDimensionDto {
+    @ApiProperty({
+        example: 'Dimension Name',
+        description: 'The Name of the Dimension',
+    })
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+    
+    @ApiProperty({
+        example: 'Dimension Description',
+        description: 'The description of the Dimension',
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    description?: string;
+    
+    @ApiProperty({
+        example: true,
+        description: 'The Features of the Dimension',
+        required: false,
+    })
+    @IsBoolean()
+    @IsOptional()
+    features: [string];
+    @ApiProperty({
+        example: 'Low',
+        description: 'The Danger Level of the Dimension',
+        type: String,
+        enum: Object.values(DangerLevelEnum),
+        default: DangerLevelEnum.LOW,
+    })
+    dangerLevel?: DangerLevelEnum;
+}
--- src/dimensions/dto/update-dimension.dto.ts
+++ src/dimensions/dto/update-dimension.dto.ts
@@ -1,4 +1,46 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsString, IsOptional, IsBoolean, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateDimensionDto } from './create-dimension.dto';
 
-export class UpdateDimensionDto extends PartialType(CreateDimensionDto) {}
+enum DangerLevelEnum {
+    LOW = 'Low',
+    MEDIUM = 'Medium',
+    HIGH = 'High',
+}
+
+export class UpdateDimensionDto extends PartialType(CreateDimensionDto) {
+    @ApiProperty({
+        example: 'Dimension Name',
+        description: 'The Name of the Dimension',
+    })
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+    
+    @ApiProperty({
+        example: 'Dimension Description',
+        description: 'The description of the Dimension',
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    description?: string;
+    
+    @ApiProperty({
+        example: true,
+        description: 'The Features of the Dimension',
+        required: false,
+    })
+    @IsBoolean()
+    @IsOptional()
+    features: [string];
+    @ApiProperty({
+        example: 'Low',
+        description: 'The Danger Level of the Dimension',
+        type: String,
+        enum: Object.values(DangerLevelEnum),
+        default: DangerLevelEnum.LOW,
+    })
+    dangerLevel?: DangerLevelEnum;
+}
