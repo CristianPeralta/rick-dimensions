4ada7cc0537c9b04753b3a9a20712ad3c0096989
Cristian Peralta <cbperaltasegura@gmail.com>
2024-01-28 17:02:51 -0500

    feat: Add dimension services

--- src/dimensions/dimensions.service.ts
+++ src/dimensions/dimensions.service.ts
@@ -1,26 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateDimensionDto } from './dto/create-dimension.dto';
 import { UpdateDimensionDto } from './dto/update-dimension.dto';
+import { Dimension } from '../schemas/dimension.schema';
 
 @Injectable()
 export class DimensionsService {
-  create(createDimensionDto: CreateDimensionDto) {
-    return 'This action adds a new dimension';
+  constructor(@InjectModel(Dimension.name) private dimensionModel: Model<Dimension>) {}
+
+  /**
+   * Retrieves all Dimensions.
+   * @returns A list of Dimensions.
+   */
+  async findAll() {
+    return this.dimensionModel.find();
   }
 
-  findAll() {
-    return `This action returns all dimensions`;
+  /**
+   * Creates a new Dimension.
+   * @param dimension The dimension data.
+   * @returns The created dimension.
+   */
+  async create(dimension: CreateDimensionDto) {
+    const newDimension = new this.dimensionModel(dimension);
+    return newDimension.save();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} dimension`;
+  /**
+   * Finds a Dimension by its ID.
+   * @param id The Dimension ID.
+   * @returns The found Dimension, or `null` if not found.
+   */
+  async findOne(id: string) {
+    return this.dimensionModel.findById(id);
   }
 
-  update(id: number, updateDimensionDto: UpdateDimensionDto) {
-    return `This action updates a #${id} dimension`;
+  /**
+   * Deletes a Dimension by its ID.
+   * @param id The Dimension ID.
+   * @returns The deleted Dimension, or `null` if not found.
+   */
+  async delete(id: string) {
+    return this.dimensionModel.findByIdAndDelete(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} dimension`;
+  /**
+   * Updates a Dimension by its ID.
+   * @param id The Dimension ID.
+   * @param dimension The updated Dimension data.
+   * @returns The updated Dimension, or `null` if not found.
+   */
+  async update(id: string, dimension: UpdateDimensionDto) {
+    return this.dimensionModel.findByIdAndUpdate(id, dimension, { new: true });
   }
 }
